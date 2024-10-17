410ff20fa4c02ff6b4b78be95c098a4f0202282c
Gaetanerk <gaetan.turben@outlook.com>
2024-10-17 14:16:06 +0200

    test ok

--- src/main/java/org/example/Exercice5/Product.java
+++ src/main/java/org/example/Exercice5/Product.java
@@ -3,6 +3,39 @@ package org.example.Exercice5;
 public class Product {
     private String type;
     private String name;
+
+    public int getSellIn() {
+        return sellIn;
+    }
+
+    public void setSellIn(int sellIn) {
+        this.sellIn = sellIn;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
     private int sellIn;
     private int quality;
 }
--- src/main/java/org/example/Exercice5/Shop.java
+++ src/main/java/org/example/Exercice5/Shop.java
@@ -1,13 +1,32 @@
 package org.example.Exercice5;
 
-import jdk.jshell.spi.ExecutionControl;
-
 import java.util.List;
 
 public class Shop {
     private List<Product> products;
 
     public void update(Product product) {
-        throw new ExecutionControl.NotImplementedException();
+        int qualityChange = -1;
+
+        if ("brie".equals(product.getType())) {
+            qualityChange = 1;
+        } else if ("dairy".equals(product.getType())) {
+            qualityChange = -2;
+        }
+
+        product.setSellIn(product.getSellIn() - 1);
+
+        if (product.getSellIn() < 0) {
+            qualityChange *= 2;
+        }
+
+        int newQuality = product.getQuality() + qualityChange;
+        if (newQuality < 0) {
+            newQuality = 0;
+        } else if (newQuality > 50) {
+            newQuality = 50;
+        }
+
+        product.setQuality(newQuality);
     }
 }
